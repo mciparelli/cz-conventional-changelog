a3c00cafb976119cd0f0453525c7843afca5e418
Martín Ciparelli <mciparelli@gmail.com>
2019-03-26 14:05:32 -0300

    update engine

--- engine.js
+++ engine.js
@@ -1,27 +1,20 @@
 "format cjs";
 
-var wrap = require('word-wrap');
-var map = require('lodash.map');
-var longest = require('longest');
-var rightPad = require('right-pad');
-
-var filter = function(array) {
-  return array.filter(function(x) {
-    return x;
-  });
-};
+var wrap = require("word-wrap");
+var map = require("lodash.map");
+var longest = require("longest");
+var rightPad = require("right-pad");
 
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
 // fine.
-module.exports = function (options) {
-
+module.exports = function(options) {
   var types = options.types;
 
   var length = longest(Object.keys(types)).length + 1;
-  var choices = map(types, function (type, key) {
+  var choices = map(types, function(type, key) {
     return {
-      name: rightPad(key + ':', length) + ' ' + type.description,
+      name: rightPad(key + ":", length) + " " + type.description,
       value: key
     };
   });
@@ -39,7 +32,9 @@ module.exports = function (options) {
     // By default, we'll de-indent your commit
     // template and will keep empty lines.
     prompter: function(cz, commit) {
-      console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
+      console.log(
+        "\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n"
+      );
 
       // Let's ask some questions of the user
       // so that we can populate our commit
@@ -50,46 +45,41 @@ module.exports = function (options) {
       // collection library if you prefer.
       cz.prompt([
         {
-          type: 'list',
-          name: 'type',
-          message: 'Select the type of change that you\'re committing:',
+          type: "list",
+          name: "type",
+          message: "Select the type of change that you're committing:",
           choices: choices,
           default: options.defaultType
-        }, {
-          type: 'input',
-          name: 'scope',
-          message: 'What is the scope of this change (e.g. component or file name)? (press enter to skip)\n',
+        },
+        {
+          type: "input",
+          name: "scope",
+          message: "What is the task number?\n",
           default: options.defaultScope
-        }, {
-          type: 'input',
-          name: 'subject',
-          message: 'Write a short, imperative tense description of the change:\n',
+        },
+        {
+          type: "input",
+          name: "subject",
+          message:
+            "Write a short, imperative tense description of the change:\n",
           default: options.defaultSubject
-        }, {
-          type: 'input',
-          name: 'body',
-          message: 'Provide a longer description of the change: (press enter to skip)\n',
+        },
+        {
+          type: "input",
+          name: "body",
+          message:
+            "Provide a longer description of the change: (press enter to skip)\n",
           default: options.defaultBody
-        }, {
-          type: 'confirm',
-          name: 'isBreaking',
-          message: 'Are there any breaking changes?',
-          default: false
-        }, {
-          type: 'input',
-          name: 'breaking',
-          message: 'Describe the breaking changes:\n',
-          when: function(answers) {
-            return answers.isBreaking;
-          }
-        }, {
-          type: 'confirm',
-          name: 'isIssueAffected',
-          message: 'Does this change affect any open issues?',
+        },
+        {
+          type: "confirm",
+          name: "isIssueAffected",
+          message: "Does this change affect any open issues?",
           default: options.defaultIssues ? true : false
-        }, {
-          type: 'input',
-          name: 'issues',
+        },
+        {
+          type: "input",
+          name: "issues",
           message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
           when: function(answers) {
             return answers.isIssueAffected;
@@ -97,36 +87,27 @@ module.exports = function (options) {
           default: options.defaultIssues ? options.defaultIssues : undefined
         }
       ]).then(function(answers) {
-
         var maxLineWidth = 100;
 
         var wrapOptions = {
           trim: true,
-          newline: '\n',
-          indent:'',
+          newline: "\n",
+          indent: "",
           width: maxLineWidth
         };
 
-        // parentheses are only needed when a scope is present
-        var scope = answers.scope.trim();
-        scope = scope ? '(' + answers.scope.trim() + ')' : '';
-
         // Hard limit this line
-        var head = (answers.type + scope + ': ' + answers.subject.trim()).slice(0, maxLineWidth);
+        var head = (
+          answers.type +
+          (answers.issues ? ` ${answers.issues}` : "") +
+          ": " +
+          answers.subject.trim()
+        ).slice(0, maxLineWidth);
 
         // Wrap these lines at 100 characters
         var body = wrap(answers.body, wrapOptions);
 
-        // Apply breaking change prefix, removing it if already present
-        var breaking = answers.breaking ? answers.breaking.trim() : '';
-        breaking = breaking ? 'BREAKING CHANGE: ' + breaking.replace(/^BREAKING CHANGE: /, '') : '';
-        breaking = wrap(breaking, wrapOptions);
-
-        var issues = answers.issues ? wrap(answers.issues, wrapOptions) : '';
-
-        var footer = filter([ breaking, issues ]).join('\n\n');
-
-        commit(head + '\n\n' + body + '\n\n' + footer);
+        commit(head + "\n\n" + body);
       });
     }
   };
